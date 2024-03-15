2573734eb9978a3ae5d974db589ee5f840154264
Anna Tsepilova <virgo.tsav@gmail.com>
2024-03-15 23:19:41 +0200

    add .addMatcher to slice

--- src/redux/hendlers.js
+++ src/redux/hendlers.js
@@ -2,7 +2,7 @@ export const handlePending = state => {
   state.isLoading = true;
 };
 
-export const handleFulfilled = (state, action) => {
+export const handleFulfilled = state => {
   state.isLoading = false;
   state.error = null;
 };
